fb8866f17f354fc59fc139716e86cfd98855c07d
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-07-03 00:24:41 +0700

    Routing pada php

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,16 @@
 
 class App{
     public function __construct(){
-        echo "Hello word";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+    public function parseUrl(){
+        if(isset($_GET["url"])){
+            $url =rtrim($_GET["url"],"/"); //Tidak memperbolehkan tanda / di akhir string
+            $url = filter_var($url,FILTER_SANITIZE_URL);  //Ini berfungsi menyaring dan membersihkan data URL
+            $url = explode("/",$url);
+            return $url;
+        }
+
     }
 }
