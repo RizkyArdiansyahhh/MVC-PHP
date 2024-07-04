2a8bdcf2d7445caa3a15384b095f10d0c860ca85
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-07-04 23:40:18 +0700

    Controller pada php

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+class About{
+
+    public function index($name = "Rizky ardiansyah", $work="Software Engginer"){
+        echo "Halo, Nama Saya $name, Saya Bekerja Sebagai $work";
+    }
+
+    public function page(){
+        echo "About/page";
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,42 @@
 <?php
 
 class App{
+    protected $controller = "Home";
+    protected $method = "index";
+    protected $params = [];
+
     public function __construct(){
         $url = $this->parseUrl();
-        var_dump($url);
+
+        //Controller
+        if(isset($url[0])){
+        if(file_exists("../app/controllers/" . $url[0] . ".php")){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+    }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // Params
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        // Jalankan Controller, Method, Serta kirimkan Params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
+
+
     }
     public function parseUrl(){
         if(isset($_GET["url"])){
